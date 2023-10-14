652e968c09a772268a7d7cf8d829d0313730fd23
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-10-15 01:12:32 +0300

    Refactor login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import axiosInstance from '../axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import axiosInstance from '../axios'
+import { useNavigate } from 'react-router-dom'
 //MaterialUI
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
@@ -32,27 +32,27 @@ const useStyles = makeStyles((theme) => ({
 	submit: {
 		margin: theme.spacing(3, 0, 2),
 	},
-}));
+}))
 
 const Login = () => {
-	const navigate = useNavigate();
+	const navigate = useNavigate()
 	const initialFormData = Object.freeze({
 		email: '',
 		password: '',
-	});
+	})
 
-	const [formData, updateFormData] = useState(initialFormData);
+	const [formData, updateFormData] = useState(initialFormData)
 
 	const handleChange = (e) => {
 		updateFormData({
 			...formData,
 			[e.target.name]: e.target.value.trim(),
-		});
-	};
+		})
+	}
 
 	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(formData);
+		e.preventDefault()
+		console.log(formData)
 
 		axiosInstance
 			.post(`token/`, {
@@ -60,17 +60,17 @@ const Login = () => {
 				password: formData.password,
 			})
 			.then((res) => {
-				localStorage.setItem('access_token', res.data.access);
-				localStorage.setItem('refresh_token', res.data.refresh);
+				localStorage.setItem('access_token', res.data.access)
+				localStorage.setItem('refresh_token', res.data.refresh)
 				axiosInstance.defaults.headers['Authorization'] =
-					'Bearer ' + localStorage.getItem('access_token');
-				navigate('/');
-				//console.log(res);
-				//console.log(res.data);
-			});
-	};
+					'Bearer ' + localStorage.getItem('access_token')
+				navigate('/')
+				//console.log(res)
+				//console.log(res.data)
+			}).catch(e => console.log(e.message))
+	}
 
-	const classes = useStyles();
+	const classes = useStyles()
 
 	return (
 		<Container component="main" maxWidth="xs">
@@ -102,7 +102,7 @@ const Login = () => {
 				</form>
 			</div>
 		</Container>
-	);
+	)
 }
 
 export default Login
